e067390f237076d4f8c49bb404ce2df446e67b48
cexarmonroy <cesar.monroy.a@gmail.com>
2024-10-21 23:18:15 -0300

    fix inspector

--- src/main/java/cl/ciisa/despensapp2/security/SecurityConfig.java
+++ src/main/java/cl/ciisa/despensapp2/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityChain(HttpSecurity http) throws Exception {
 		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and().authorizeHttpRequests()
-				.requestMatchers("/login","/users/register/**","/users/check-username/**","/resources/**", "/css/**", "/assets/**", "/dist/**", "/src/**").permitAll()
+				.requestMatchers("/login","/users/register/**","/users/check-username/**","/resources/**", "/css/**", "/assets/**", "/dist/**", "/src/**","/error").permitAll()
 				.anyRequest().authenticated().and().formLogin().loginPage("/login").defaultSuccessUrl("/")
 				.failureUrl("/login?error").permitAll().and().logout().logoutUrl("/logout").permitAll();
 
